839fe092b2be82c50810203e19f520196487da5d
Napoleon Arouldas <na@grandappstudio.com>
2021-03-10 10:43:08 +0530

    add sql, readme improvised

--- mysql.sql
+++ mysql.sql
@@ -1,22 +1,16 @@
 # Mysql instant usable snippets
 
 --- Create database
-
-
 create database ecommerce_db;
 
 
 --- List all database
-
-be watchful it is databases (plural) not database 
-
+---be watchful it is databases (plural) not database 
 
 show databases;
 
 
 --- Create table with primary key, 
-
-
 create table people ( 
 		person_id INT AUTO_INCREMENT PRIMARY KEY , 
 		full_name varchar(50),
