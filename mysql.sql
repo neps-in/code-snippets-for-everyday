9febe53db12d9767da659c56835465453a386a8b
Napoleon Arouldas <na@grandappstudio.com>
2021-03-10 10:46:17 +0530

    add sql, readme improvised

--- mysql.sql
+++ mysql.sql
@@ -1,16 +1,24 @@
 # Mysql instant usable snippets
 
---- Create database
+# Llist of valid comments in mysql sql file
+# Valide comment style 1
+-- double dash comment style 2
+/*  
+	C Style comment that is 
+	multi line comment 
+*/
+
+-- Create database
 create database ecommerce_db;
 
 
---- List all database
----be watchful it is databases (plural) not database 
+-- List all database
+-- be watchful it is databases (plural) not database 
 
 show databases;
 
 
---- Create table with primary key, 
+-- Create table with primary key, 
 create table people ( 
 		person_id INT AUTO_INCREMENT PRIMARY KEY , 
 		full_name varchar(50),
